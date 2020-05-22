d16b76141f217087f4b66017e569780e3e74f2ea
Alasdair McLeay <alasdair.mcleay@me.com>
2020-05-22 15:10:32 +0100

    argument variables as decibels

--- index.js
+++ index.js
@@ -16,33 +16,42 @@ export const ID_XU_OUT = 51; /* 0x33 */
 export const ID_XU_IN = 52; /* 0x34 */
 export const ID_MIXER_1 = 60; /* 0x3C */
 
-export const OUT = bmRequestType(DIRECTION.Out, TYPE.Class, RECIPIENT.Interface);
+export const OUT = bmRequestType(
+  DIRECTION.Out,
+  TYPE.Class,
+  RECIPIENT.Interface
+);
 export const IN = bmRequestType(DIRECTION.In, TYPE.Class, RECIPIENT.Interface);
 
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
 const dataToBuffer = (data) => {
-    if (data < 0 || data > 0xffff) {
-      throw new Error("Data out of bounds");
-    }
-    return new Uint8Array([(data >> 8) & 0xff, data & 0xff]);
-  };
+  if (data < -0x8000 || data > 0xffff) {
+    throw new Error("Data out of bounds. Unsigned values should be between 0x0000 and 0xffff, signed values should be between -0x8000 and 0x7fff");
+  }
+  return new Uint8Array([data & 0xff, (data >> 8) & 0xff]);
+};
 
 // return controlTransfer that are compatible with lib usb (e.g. via https://github.com/tessel/node-usb)
-export const volume = (level, bus = 0) => {
+// argument variables are in decibels, USB Audio uses a signed 32 bit int in increments of 1/256 db
+export const volume = (v, bus = 0, min = -128, max=0) => {
   return {
     bmRequestType: OUT,
     bRequest: CUR,
     wValue: (FU_VOLUME_CONTROL << 8) + bus,
     wIndex: FU_USBOUT << 8,
-    data: dataToBuffer(level),
+    data: dataToBuffer(clamp(v, min, max) << 8),
   };
 };
 
-export const mixer = (gain, node = 1) => {
+export const mixer = (gain, node = 1, min = -128, max=0) => {
   return {
     bmRequestType: OUT,
     bRequest: CUR,
     wValue: node,
     wIndex: ID_MIXER_1 << 8,
-    data: dataToBuffer(gain),
+    data: dataToBuffer(clamp(gain, min, max) << 8),
   };
 };
